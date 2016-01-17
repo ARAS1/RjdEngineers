396f1f38d3c23db9d2d85886e0ba07f92211de18
ARAS1 <aras2013@gmail.com>
2016-01-17 16:32:26 +0000

    Revert "DB connection"
    This reverts commit b98196396c75b5ca01e4ad5f4ae28b17bcf62e2f.

--- controler/Register.php
+++ controler/Register.php
@@ -2,39 +2,18 @@
 include '../model/Account.php';
 if (isset($_POST['submitBtn'])){   
 $model = new Account();  
-$model->firstName = htmlspecialchars($_POST['firstNameFd']);
-$model->lastName = htmlspecialchars($_POST['lastNameFd']);
-$model->userName = htmlspecialchars($_POST['userNameFd']);
-$model->emailAddress = htmlspecialchars($_POST['emailAddressFd']);
-$model->addressLine1 = htmlspecialchars($_POST['addressLine1Fd']);
-$model->addressLine2 = htmlspecialchars($_POST['addressLine2Fd']);
-$model->cityTown = htmlspecialchars($_POST['cityTownFd']);
-$model->country = htmlspecialchars($_POST['countryFd']);
-$model->postCode = htmlspecialchars($_POST['postCodeFd']);
-$model->password = htmlspecialchars($_POST['passwordFd']);
-$model->confirmPassword = htmlspecialchars($_POST['confirmPasswordFd']);
+$model->firstName = $_POST['firstNameFd'];
+$model->lastName = $_POST['lastNameFd'];
+$model->userName = $_POST['userNameFd'];
+$model->emailAddress = $_POST['emailAddressFd'];
+$model->addressLine1 = $_POST['addressLine1Fd'];
+$model->addressLine2 = $_POST['addressLine2Fd'];
+$model->cityTown = $_POST['cityTownFd'];
+$model->country = $_POST['countryFd'];
+$model->postCode = $_POST['postCodeFd'];
+$model->password = $_POST['passwordFd'];
+$model->confirmPassword = $_POST['confirmPasswordFd'];
 }
-
-$passwordHash = password_hash($model->password, PASSWORD_DEFAULT);
-$db = mysqli_connect('192.168.1.64', 'CoffeeAppUser', 'TyUEMqZncSdj6n4K', 'Coffee&Chocolate');
-$sql = sprintf("INSERT INTO Account (First_Name, Last_Name, User_Name, Email_Address, Address_Line1, Address_Line2, 
-    City_Town, Country, Country_Code, Post_Code, Password_Hash, Is_Active, Creation_Date, Last_Login) VALUES (
- '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'
-        )", mysqli_real_escape_string($db, $model->firstName),
-            mysqli_real_escape_string($db, $model->lastName),
-            mysqli_real_escape_string($db, $model->userName),
-            mysqli_real_escape_string($db, $model->emailAddress),
-            mysqli_real_escape_string($db, $model->addressLine1),
-            mysqli_real_escape_string($db, $model->addressLine2),
-            mysqli_real_escape_string($db, $model->cityTown),
-            mysqli_real_escape_string($db, $model->country),
-            mysqli_real_escape_string($db, $model->postCode),
-            mysqli_real_escape_string($db, $model->cityTown),
-            mysqli_real_escape_string($db, $passwordHash));
-mysqli_query($db, $sql);
-mysqli_close($db);
-
-
 
  echo $model->firstName;
  echo $model->lastName;
@@ -47,6 +26,5 @@ mysqli_close($db);
  echo $model->postCode;
  echo $model->password;
  echo $model->confirmPassword;
- echo $passwordHash;
- 
+  
 ?>
