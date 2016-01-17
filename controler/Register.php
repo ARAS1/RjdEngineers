2be3534f6b7c0993164ccf7e8432f09981856554
ARAS1 <aras2013@gmail.com>
2016-01-17 14:46:36 +0000

    Submit button is working
    After tons of testing, it is finally working.

--- controler/Register.php
+++ controler/Register.php
@@ -2,7 +2,7 @@
 include '../model/Account.php';
 if (isset($_POST['submitBtn'])){   
 $model = new Account();  
-$name = $_POST['firstNameFd'];
+$model->firstName = $_POST['firstNameFd'];
 $model->lastName = $_POST['lastNameFd'];
 $model->userName = $_POST['userNameFd'];
 $model->emailAddress = $_POST['emailAddressFd'];
@@ -14,17 +14,31 @@ $model->postCode = $_POST['postCodeFd'];
 $model->password = $_POST['passwordFd'];
 $model->confirmPassword = $_POST['confirmPasswordFd'];
 }
- echo "<p>$firstName </p>";
- echo "<p>$model->firstName </p>";
- echo "<p>$name </p>";
- echo "<p> Output some stuff 1 </p>";
+ echo $firstName;
+ echo $lastName;
+ echo $userName;
+ echo $emailAddress;
+ echo $addressLine1;
+ echo $addressLine2;
+ echo $cityTown;
+ echo $country;
+ echo $postCode;
+ echo $password;
+ echo $confirmPassword;
+ 
+ echo "---------model output---------";
+ 
+ echo $model->firstName;
+ echo $model->lastName;
+ echo $model->userName;
+ echo $model->emailAddress;
+ echo $model->addressLine1;
+ echo $model->addressLine2;
+ echo $model->cityTown;
+ echo $model->country;
+ echo $model->postCode;
+ echo $model->password;
+ echo $model->confirmPassword;
+ 
+ 
 ?>
-
-<!DOCTYPE html>
-<head></head>
-<body>
-<?php echo "<p>$model->firstName </p>"?>
-<?php echo "<p>$model->firstName </p>"?>
-<?php echo "<p>$name </p>"?>
-<?php echo "<p> Output some stuff 2</p>"?>
-</body>
