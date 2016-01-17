6a89a312b685b90b794c36269fccb841336ef3c7
ARAS1 <aras2013@gmail.com>
2016-01-17 14:04:52 +0000

    Still working on php registration submit button

--- controler/Register.php
+++ controler/Register.php
@@ -1,27 +1,25 @@
 <?php
 include '../model/Account.php';
-if ($_SERVER['submitBtn'] == 'POST'){   
+if (isset($_POST['Submit1'])){   
 $model = new Account();  
-$model ->firstName = $_POST['firstNameFd'];
-$model ->lastName = $_POST['lastNameFd'];
-$model ->userName = $_POST['userNameFd'];
-$model ->emailAddress = $_POST['emailAddressFd'];
-$model ->addressLine1 = $_POST['addressLine1Fd'];
-$model ->addressLine2 = $_POST['addressLine2Fd'];
-$model ->cityTown = $_POST['cityTownFd'];
-$model ->country = $_POST['countryFd'];
-$model ->postCode = $_POST['postCodeFd'];
-$model ->password = $_POST['passwordFd'];
-$model ->confirmPassword = $_POST['confirmPasswordFd'];
+$model->firstName = $_POST['firstNameFd'];
+$model->lastName = $_POST['lastNameFd'];
+$model->userName = $_POST['userNameFd'];
+$model->emailAddress = $_POST['emailAddressFd'];
+$model->addressLine1 = $_POST['addressLine1Fd'];
+$model->addressLine2 = $_POST['addressLine2Fd'];
+$model->cityTown = $_POST['cityTownFd'];
+$model->country = $_POST['countryFd'];
+$model->postCode = $_POST['postCodeFd'];
+$model->password = $_POST['passwordFd'];
+$model->confirmPassword = $_POST['confirmPasswordFd'];
 }
 ?>
 
 <!DOCTYPE html>
 <head></head>
 <body>
-    <?php echo $modelfirstName;?></br>
-    <?php echo $model;?></br>
-    <?php var_dump($model);?></br>
-    <?php echo $firstName;?></br>
-    
+<?php echo "<p>$model->firstName </p>"?>
+<?php echo "<p>$model->firstName </p>"?>
+<?php echo "<p> Output some stuff</p>"?>
 </body>
